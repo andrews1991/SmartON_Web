86cdff31de1e3ff7742f98f59d78932d6e01f5d6
Prakash <Prakash@Prakash-PC>
2019-09-14 17:26:00 +0530

    Commit test

--- StudentPortal/src/main/java/com/protean/student/StudentPortal/StudentPortalApplication.java
+++ StudentPortal/src/main/java/com/protean/student/StudentPortal/StudentPortalApplication.java
@@ -3,7 +3,7 @@ package com.protean.student.StudentPortal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication    
+@SpringBootApplication      
 public class StudentPortalApplication {
 
 	public static void main(String[] args) {
