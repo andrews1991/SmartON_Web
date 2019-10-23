4a7b7f1b18af5b03aef0e57e104875296927c064
jojus <itsjustinjoy@gmail.com>
2019-10-24 00:11:16 +0530

    sending mail to user after register success

--- StudentPortal/src/main/java/com/protean/student/StudentPortal/service/MailSenderService.java
+++ StudentPortal/src/main/java/com/protean/student/StudentPortal/service/MailSenderService.java
@@ -13,25 +13,27 @@ import com.protean.student.StudentPortal.model.RegisterUserDetails;
 public class MailSenderService {
 
 	@Autowired
-	private JavaMailSender jms; 
-	
+	private JavaMailSender jms;
+
 	@Autowired
 	public MailSenderService(JavaMailSender javaMailSender) {
 		this.jms = javaMailSender;
 	}
-	
+
 	public void sendEmail(RegisterUserDetails registerUserDetails) throws MessagingException {
 		SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo(registerUserDetails.getEmail());
+		msg.setTo(registerUserDetails.getEmail());
 
-        msg.setSubject("Thank You for Register in Tag #");
-        String body = "Welcome to” Tag “The Professional Academy \r\n" + 
-        		"TAG a unique concept in bridging your career in line to current corporate requirement.\r\n" + 
-        		"Freedom to design our own life is the greatest gift we can ever get and TAG academy is the right platform to help you get closer to it.\r\n" + 
-        		"We are glad you choose TAG !!\r\n" + 
-        		"Welcome to the Academy !!";
-        body+="\n \n Use this Referal code "+registerUserDetails.getProfileID()+" and You have Earn Rewards! "+registerUserDetails.getRewpoints()*1000+" Happy Learning!";
-        msg.setText(body);
-        jms.send(msg); 
+		msg.setSubject("Thank You for Register in Tag #");
+
+		String body = "Welcome to” Tag “The Professional Academy \r\n"
+				+ "TAG a unique concept in bridging your career in line to current corporate requirement.\r\n"
+				+ "Freedom to design our own life is the greatest gift we can ever get and TAG academy is the right platform to help you get closer to it.\r\n"
+				+ "We are glad you choose TAG !!\r\n" + "Welcome to the Academy !!";
+		body += "\n \n Use this Referal code " + registerUserDetails.getProfileID() + " and You have Earn Rewards!  '1000' " + " Happy Learning!";
+
+		msg.setText(body);
+		jms.send(msg);
+
 	}
 }
